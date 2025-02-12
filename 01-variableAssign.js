b0d6441e933318e6f20a80daa0673ef71a589ae3
sohebshaikh1122 <sohebs728@gmail.com>
2025-02-12 09:17:10 +0530

    01-Assignment_JS
    01-Assignment_JS

---
+++ 01-variableAssign.js
@@ -0,0 +1,37 @@
+var myFullName;
+console.log("My full name with no value:",myFullName);
+
+myFullName="soheb shaikh";
+console.log("Fisrt and last name:",myFullName);
+
+myFullName="soheb shokat shaikh";
+console.log("Full name:",myFullName);
+
+var myLove="I Love Only JS";
+console.log("favourite subject:",myLove);
+
+var firstName="soheb";
+console.log("First name:",firstName);
+
+var lastName="shaikh";
+console.log("Last Name:",lastName);
+
+var age=25;
+console.log("Age:",age);
+
+var address="pune";
+console.log("My address:",address);
+
+var pincode=413114;
+console.log("My pincode:",pincode);
+
+var gender ="male";
+console.log("The gender:",gender);
+
+var isMarried="No"
+console.log("Married Status:",isMarried);
+
+
+
+
+
