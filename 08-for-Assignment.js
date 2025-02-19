081690f2fbbf3bd2297256dca72847449c434767
sohebshaikh1122 <sohebs728@gmail.com>
2025-02-19 23:54:14 +0530

    08-Assignment - for loop
    08-Assignment - for loop

---
+++ 08-for-Assignment.js
@@ -0,0 +1,39 @@
+console.log(`----print number from 5 to 15 by incrementing 1-----`);
+for (var index = 5; index <=15; index++) 
+{
+    console.log(`${index}`);
+    
+}
+console.log(`----print number from 50 to 40 by decrementing 1-----`);
+for (var index = 50; index>=40; index--) 
+{
+   console.log(`${index}`);
+    
+}
+console.log(`----print number first 15 odd number -----`);
+for (var index = 1; index <=30; index=index+2) 
+{
+   console.log(`${index}`);
+        
+}
+console.log(`----print number first 10 even number -----`);
+for (var index = 0; index <=20; index=index+2) 
+{
+  console.log(`${index}`);
+            
+}
+console.log(`----print table of 5 like 5-10- 15----`);
+for (var index = 5; index <=50; index=index+5) 
+{
+  console.log(`${index}`);     
+}
+console.log(`----print table of 10 like 10-20-30---`);
+for (var index = 10; index <=100; index=index+10) 
+{
+  console.log(`${index}`);       
+}
+console.log(`----print table of 10 in reverse oredr like 100- 90- 80-`);
+for (var index = 100; index>=10; index=index-10) 
+{
+  console.log(`${index}`);       
+}
