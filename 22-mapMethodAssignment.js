4f34477ef8d731b2c49d4102adfd57d69d45039e
sohebshaikh1122 <sohebs728@gmail.com>
2025-03-06 12:38:29 +0530

    Assignment 22 Map methods
    Assignment 22 Map methods

---
+++ 22-mapMethodAssignment.js
@@ -0,0 +1,39 @@
+console.log(`========================= Task 1 ==============================`);
+console.log(`Given Array :- [20,11,40,25,23,11,9,31,60,2,19]`);
+console.log(`---log the array adding 10 in each element using map arrow function------`);
+
+const arrayNumber =[20,11,40,25,23,11,9,31,60,2,19];
+const result = new Map(); 
+
+const ans=arrayNumber.map(element => {
+
+    element=element+10;
+    return element;
+});
+console.log(ans);
+
+console.log(`========================= Task 2 ==============================`);
+console.log(`Given Array :- [20,11,40,25,23,11,9,31,60,2,19]`);
+console.log(`----------- cube of the array using map arrow function--------`);
+
+const cube=arrayNumber.map((Element)=>
+{
+    
+    return Element * Element * Element;
+})
+console.log(cube);
+
+console.log(`========================= Task 3 ==============================`);
+console.log(`Given Array :- [20,11,40,25,23,11,9,31,60,2,19]`);
+console.log(`-----add element and index into the array using map arrow function----`);
+const addtion = arrayNumber.map((Element,index)=>
+{
+    return index + Element ;
+})
+console.log(addtion);
+
+
+
+
+
+
