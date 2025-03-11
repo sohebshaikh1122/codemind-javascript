6c738a7a99736bbe3d3931970c3e4a080f1d2b4b
sohebshaikh1122 <sohebs728@gmail.com>
2025-03-11 22:48:22 +0530

    Assignment-26-DOM- Style elements
    Assignment-26-DOM- Style elements

---
+++ 26-2-dom-styleAssignment.js
@@ -0,0 +1,10 @@
+console.log(`=====Giving style to h1 element using ID attribute========`);
+console.log(` My profile Details`);
+const h1_element=document.querySelector("#h1");
+h1_element.style.color="Green";
+
+console.log(`=====giving style to h2 element using element attribute =======`);
+console.log(`Technology Stack`);
+const h2_element=document.querySelector("h2");
+h2_element.style.color="red";
+
